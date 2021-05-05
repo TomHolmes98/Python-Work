2dfac16174614114e787360bb948135389ac399d
Thomas Holmes <82601909+TomHolmes98@users.noreply.github.com>
2021-05-05 01:33:04 +0100

    Delete even-number-challenge.py

--- Challenges/even-number-challenge.py
+++
@@ -1,7 +0,0 @@
-def print_even_numbers(num_one, num_two):
-    for number in range(num_one, num_two):
-        if number % 2 == 0:
-            even_numbers.append(number)
-    return even_numbers
-
-print(print_even_numbers(20, 40))
